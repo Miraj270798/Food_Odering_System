80a62565f93234bf3bdd79f2bd5fc8c5eea5ff9b
Miraj <raval.miraj@gmail.com>
2022-03-06 19:27:57 +0530

    first commit

---
+++ fos/admin/includes/footer.php
@@ -0,0 +1,6 @@
+<div class="footer">
+            
+            <div>
+                <strong>Copyright @</strong> Food Ordering System &copy; 2021-2022
+            </div>
+        </div>
---
+++ fos/includes/footer.php
@@ -0,0 +1,54 @@
+ <footer class="footer">
+               <div class="container">
+                  <!-- top footer statrs -->
+                  <div class="row top-footer">
+                     <div class="col-xs-12 col-sm-3 footer-logo-block color-gray">
+                        <a href="index.php" style="font-size:20px;"> Food Ordering System </a><br /> <span>Order Delivery  </span> 
+                     </div>
+                     <div class="col-xs-12 col-sm-2 about color-gray">
+                        <h5>About Us</h5>
+                        <ul>
+                           <li><a href="about-us.php">About us</a> </li>
+                           <li><a href="contact.php">Contact us</a> </li>
+                       
+                        </ul>
+                     </div>
+                     <div class="col-xs-12 col-sm-2 how-it-works-links color-gray">
+                        <h5>My Account</h5>
+                        <ul>
+                           <li><a href="#">My Profile</a> </li>
+                           <li><a href="#">My Cart</a> </li>
+                           <li><a href="#">My Order</a> </li>
+                        
+                        </ul>
+                     </div>
+        <div class="col-xs-12 col-sm-2 how-it-works-links color-gray">
+                        <h5>Track Order </h5>
+                        <ul>
+                           <li><a href="track-order-on.php">Track Order </a> </li>
+                       
+                        
+                        </ul>
+                     </div>
+
+                
+                <div class="col-xs-12 col-sm-2 how-it-works-links color-gray">
+                        <h5>Admin </h5>
+                        <ul>
+                           <li><a href="admin" target="_blank">Login </a> </li>
+                       
+                        
+                        </ul>
+                     </div>
+                
+                
+                  </div>
+                  <div align="center">
+                  
+                <strong>Copyright @</strong> Food Ordering System &copy; 2021-2022
+            </div>
+                  <!-- top footer ends -->
+  
+                  <!-- bottom footer ends -->
+               </div>
+            </footer>
